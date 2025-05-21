f30515da09f2a78342345fc14536192b7ccf01bc
Antoine Moinet <antoine_moinet@hotmail.fr>
2025-05-21 22:28:30 +0200

    update

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 EMBEDDING_MODEL = "text-embedding-3-small"
 CHAT_MODEL = "gpt-4o"
 CHUNK_SIZE = 500
-CHUNK_OVERLAP = 50
+CHUNK_OVERLAP = 3   # 3 sentences overlap
 SIMILAR_CHUNKS = 5
 
 # define absolute path for vector database
